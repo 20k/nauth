938bb038d62f401f809b5229b876980684b4a442
James <Icedshot@gmail.com>
2019-05-10 13:24:42 +0100

    beginnings of auth struct

---
+++ auth.cpp
@@ -0,0 +1,7 @@
+#include <networking/serialisable.hpp>
+#include "auth.hpp"
+
+void auth::serialise(serialise_context& ctx, nlohmann::json& data, self_t* other)
+{
+
+}
---
+++ auth.hpp
@@ -0,0 +1,11 @@
+#ifndef AUTH_HPP_INCLUDED
+#define AUTH_HPP_INCLUDED
+
+#include <networking/serialisable_fwd.hpp>
+
+struct auth
+{
+    SERIALISE_SIGNATURE();
+};
+
+#endif // AUTH_HPP_INCLUDED
