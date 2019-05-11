a2184db4ee0b564dab15649b7073f076e976f507
James <Icedshot@gmail.com>
2019-05-11 11:58:45 +0100

    auth tests

--- main.cpp
+++ main.cpp
@@ -14,7 +14,36 @@ void auth_tests()
         db_read reader(db, 0);
 
         auth<int> test;
-        assert(test.load("20k", reader) == false);
+        assert(test.load("dummy", reader) == false);
+    }
+
+    {
+        {
+            db_read_write tx(db, 0);
+
+            auth<int> test;
+            test.key = "i20k";
+            test.user_id = "01234567";
+            test.type = auth_type::CUSTOM;
+
+            test.save(tx);
+        }
+
+        {
+            db_read tx(db, 0);
+
+            auth<int> test;
+
+            assert(test.load("i20k", tx) && test.user_id == "01234567" && test.type == auth_type::CUSTOM);
+
+            std::cout << "hello there " << test.user_id << std::endl;
+        }
+
+        {
+            db_read_write tx(db, 0);
+
+            tx.del("i20k");
+        }
     }
 }
 
